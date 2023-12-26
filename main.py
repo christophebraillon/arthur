7e890805cd52f31f33ac76caadec68c15be268ca
christophebraillon <39823897+christophebraillon@users.noreply.github.com>
2023-12-26 18:26:29 +0100

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ music._play_default_background(music.built_in_playable_melody(Melodies.POWER_UP)
 vaisseau_x = 2
 compteur = 0
 niveau = 500
+arret = 0
 
 def on_forever():
     global missile_x, missile_y, compteur, niveau
@@ -14,6 +15,8 @@ def on_forever():
         led.plot(missile_x, missile_y)
         basic.pause(niveau)
         led.unplot(missile_x, missile_y)
+        while arret != 0:
+            pass
         missile_y += 1
     if missile_x == vaisseau_x:
         music.play(music.builtin_playable_sound_effect(soundExpression.giggle),
@@ -34,12 +37,28 @@ def on_forever():
 basic.forever(on_forever)
 
 def on_forever2():
-    global vaisseau_x
+    global vaisseau_x, arret
     led.unplot(vaisseau_x, 4)
-    if input.button_is_pressed(Button.A) and vaisseau_x != 0:
-        vaisseau_x += -1
-    if input.button_is_pressed(Button.B) and vaisseau_x != 4:
-        vaisseau_x += 1
-    led.plot(vaisseau_x, 4)
-    basic.pause(100)
+    if arret == 0:
+        if input.button_is_pressed(Button.A) and vaisseau_x != 0:
+            vaisseau_x += -1
+        if input.button_is_pressed(Button.B) and vaisseau_x != 4:
+            vaisseau_x += 1
+        led.plot(vaisseau_x, 4)
+        basic.pause(100)
+    if input.button_is_pressed(Button.AB):
+        if arret != 0:
+            arret = 0
+            basic.clear_screen()
+            basic.pause(1000)
+        else:
+            arret = 1
+            basic.show_leds("""
+                . # . # .
+                . # . # .
+                . # . # .
+                . # . # #
+                . # . # .
+                """)
+            basic.pause(1000)
 basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), musi
 let vaisseau_x = 2
 let compteur = 0
 let niveau = 500
+let arret = 0
 basic.forever(function () {
     missile_x = randint(0, 4)
     missile_y = 0
@@ -12,6 +13,9 @@ basic.forever(function () {
         basic.pause(niveau)
         led.unplot(missile_x, missile_y)
         missile_y += 1
+        while (arret != 0) {
+            basic.pause(100)
+        }
     }
     if (missile_x == vaisseau_x) {
         music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.InBackground)
@@ -22,8 +26,10 @@ basic.forever(function () {
         basic.clearScreen()
         basic.showIcon(IconNames.Sad)
         basic.pause(1000)
+        basic.clearScreen()
         basic.showString("" + (compteur))
         basic.pause(1000)
+        basic.clearScreen()
         basic.showString("Game over!")
         basic.pause(1000)
         control.reset()
@@ -31,12 +37,32 @@ basic.forever(function () {
 })
 basic.forever(function () {
     led.unplot(vaisseau_x, 4)
-    if (input.buttonIsPressed(Button.A) && vaisseau_x != 0) {
-        vaisseau_x += -1
+    if (arret == 0) {
+        if (input.buttonIsPressed(Button.A) && vaisseau_x != 0) {
+            vaisseau_x += -1
+        }
+        if (input.buttonIsPressed(Button.B) && vaisseau_x != 4) {
+            vaisseau_x += 1
+        }
+        led.plot(vaisseau_x, 4)
+        basic.pause(100)
     }
-    if (input.buttonIsPressed(Button.B) && vaisseau_x != 4) {
-        vaisseau_x += 1
+    if (input.buttonIsPressed(Button.AB)) {
+        if (arret != 0) {
+            basic.clearScreen()
+            basic.pause(1000)
+            arret = 0
+        } else {
+            arret = 1
+            basic.clearScreen()
+            basic.showLeds(`
+                . # . # .
+                . # . # .
+                . # . # .
+                . # . # .
+                . # . # .
+                `)
+            basic.pause(1000)
+        }
     }
-    led.plot(vaisseau_x, 4)
-    basic.pause(100)
 })
